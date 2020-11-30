a35453c7719fffa55d4071c9855fb8c93c569018
elasticnet12345 <elasticnet1995@gmail.com>
2020-11-30 14:23:35 +0900

    daily commit (2020/11/30 14:23:35)

--- daily.sh
+++ daily.sh
@@ -1,16 +1,20 @@
 set -eu
 target="/home/gesogeso/デスクトップ/grass-grower"
-a=$(date "+%Y/%m/%d %H:%M:%S")
-echo "date:"$a
-echo $a > $target"/hoge.txt"
-b=$(sha256sum $target/hoge.txt)
-echo "hash:"$b
-target_file=$target/hoge.txt
-c="$a ${b/$target_file/}"
-echo "output:"$c
-echo $c >> $target"/daily.txt"
-cd $target
-git add $target"/*"
-git status
-git commit -m "daily commit ($a)"
+
+for i in $(($RANDOM % 10))
+do
+	a=$(date "+%Y/%m/%d %H:%M:%S");
+	echo "date:"$a;
+	echo $a > $target"/hoge.txt";
+	b=$(sha256sum $target/hoge.txt);
+	echo "hash:"$b;
+	target_file=$target/hoge.txt;
+	c="$a ${b/$target_file/}";
+	echo "output:"$c;
+	echo $c >> $target"/daily.txt";
+	cd $target;
+	git add $target"/*";
+	git status;
+	git commit -m "daily commit ($a)";
+done
 git push origin main
