422298ee2aef9627c2e090339f2b97cd2401fbb0
elasticnet12345 <elasticnet1995@gmail.com>
2020-12-02 00:02:33 +0900

    fix

--- fake.sh
+++ fake.sh
@@ -1,9 +1,9 @@
 start="2020-01-01"
-for i in $(seq 1 5)
+for i in $(seq 1 100)
 do
 	d=$(date -R -d "${start} + ${i}days")
 	echo $d >> piyo.txt
 	git add piyo.txt
 	git commit -m "hogehoge"
-	git commit --ammend --date="${d}" -m "daily commit ${d}"
+	git commit --amend --date="${d}" -m "daily commit ${d}"
 done
