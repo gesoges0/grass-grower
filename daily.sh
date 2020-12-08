ee3c8964cb3352218fa2ecd20ba462a9d9a4bb13
elasticnet12345 <elasticnet1995@gmail.com>
2020-12-08 20:10:12 +0900

    daily commit (2020/12/08 20:10:12)

--- daily.sh
+++ daily.sh
@@ -1,20 +1,21 @@
 set -eu
 target="/home/gesogeso/デスクトップ/grass-grower"
-for i in $(seq 1 $(($RANDOM % 5)))
-do
-	echo $i
-	echo "============================"
-	a=$(date "+%Y/%m/%d %H:%M:%S")
-	echo "date:"$a
-	echo $a > $target"/hoge.txt"
-	b=$(sha256sum $target/hoge.txt)
-	echo "hash:"$b
-	target_file=$target/hoge.txt
-	c="$a ${b/$target_file/}"
-	echo "output:"$c
-	echo $c >> $target"/daily.txt"
-	cd $target
-	git add $target"/*"
-	git commit -m "daily commit ($a)"
-done
-git push origin master
+pushd ${target}
+	for i in $(seq 2 $(($RANDOM % 5)))
+	do
+		echo $i
+		echo "============================"
+		a=$(date "+%Y/%m/%d %H:%M:%S")
+		echo "date:"$a
+		echo $a > $target"/hoge.txt"
+		b=$(sha256sum $target/hoge.txt)
+		echo "hash:"$b
+		target_file=$target/hoge.txt
+		c="$a ${b/$target_file/}"
+		echo "output:"$c
+		echo $c >> $target"/daily.txt"
+		git add $target"/*"
+		git commit -m "daily commit ($a)"
+	done
+	git push origin master
+popd
